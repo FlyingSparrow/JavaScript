fd7f23f086c5938ccb72d3e9365a4fc1929db9cd
wjc <wjc@huishu.com.cn>
2017-04-22 10:39:28 +0800

    优化代码

--- ImageGallery/WebContent/js/public/showPic.js
+++ ImageGallery/WebContent/js/public/showPic.js
@@ -6,10 +6,29 @@ function showPic(whichPic){
 	var text = whichPic.getAttribute('title');
 	var description = document.getElementById('description');
 	description.firstChild.nodeValue = text;
-	console.log('description nodeValue='+description.nodeValue);
-	console.log('description[0] nodeValue='+description.childNodes[0].nodeValue);
-	console.log('description[0] nodeValue='+description.firstChild.nodeValue);
-	
+//	console.log('description nodeValue='+description.nodeValue);
+//	console.log('description[0] nodeValue='+description.childNodes[0].nodeValue);
+//	console.log('description[0] nodeValue='+description.firstChild.nodeValue);
+//	
 //	var body_element = document.getElementsByTagName('body')[0];
 //	console.log(body_element.nodeType);
+}
+
+function popUp(winURL){
+	window.open(winURL, 'popup', 'width=320,height=480');
+}
+
+window.onload = function(){
+	if(!document.getElementsByTagName){
+		return false;
+	}
+	var lnks = document.getElementsByTagName('a');
+	for(var i=0; i<lnks.length; i++){
+		if(lnks[i].getAttribute('class') == 'popup'){
+			lnks[i].onclick = function(){
+				popUp(this.getAttribute('href'));
+				return false;
+			}
+		}
+	}
 }

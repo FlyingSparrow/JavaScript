e4efaa1e32fe394335d98ddfdf792ec224b15ea5
wjc <wjc@huishu.com.cn>
2017-04-22 11:37:00 +0800

    优化代码

--- ImageGallery/WebContent/js/public/showPic.js
+++ ImageGallery/WebContent/js/public/showPic.js
@@ -1,17 +1,26 @@
 function showPic(whichPic){
-	var source = whichPic.getAttribute('href');
 	var placeholder = document.getElementById('placeholder');
+	if(!placeholder){
+		return false;
+	}
+	if(placeholder.nodeName != 'IMG'){
+		return false;
+	}
+	var source = whichPic.getAttribute('href');
 	placeholder.setAttribute('src', source);
 	
-	var text = whichPic.getAttribute('title');
+	var text = '';
+	if(whichPic.getAttribute('title')){
+		text = whichPic.getAttribute('title');
+	}
 	var description = document.getElementById('description');
-	description.firstChild.nodeValue = text;
-//	console.log('description nodeValue='+description.nodeValue);
-//	console.log('description[0] nodeValue='+description.childNodes[0].nodeValue);
-//	console.log('description[0] nodeValue='+description.firstChild.nodeValue);
-//	
-//	var body_element = document.getElementsByTagName('body')[0];
-//	console.log(body_element.nodeType);
+	if(description){
+		if(description.firstChild.nodeType == 3){
+			//如果是文本节点
+			description.firstChild.nodeValue = text;
+		}
+	}
+	return true;
 }
 
 function popUp(winURL){
@@ -32,3 +41,36 @@ window.onload = function(){
 		}
 	}
 }
+
+function prepareGallery(){
+	if(!document.getElementById){
+		return false;
+	}
+	if(!document.getElementsByTagName){
+		return false;
+	}
+	var gallery = document.getElementById('imagegallery');
+	if(!gallery){
+		return false;
+	}
+	var links = gallery.getElementsByTagName('a');
+	for(var i=0; i<links.length; i++){
+		links[i].onclick = function(){
+			return showPic(this) ? false : true;
+		}
+	}
+}
+
+function addLoadEvent(func){
+	var oldonload = window.onload;
+	if(typeof window.onload != 'function'){
+		window.onload = func;
+	}else{
+		window.onload = function(){
+			oldonload();
+			func();
+		}
+	}
+}
+
+addLoadEvent(prepareGallery);

41b05c75365e0bb7589747d8484c0468fac48b67
wjc <wjc@huishu.com.cn>
2017-05-05 14:18:29 +0800

    完善contact.html页面的功能

--- ImageGallery/WebContent/band/js/global.js
+++ ImageGallery/WebContent/band/js/global.js
@@ -409,6 +409,112 @@ function prepareForms(){
 	for(var i=0; i<document.forms.length; i++){
 		var thisform = document.forms[i];
 		resetFields(thisform);
+		thisform.onsubmit = function(){
+			if(!validateForm(this)){
+				return false;
+			}
+			var article = document.getElementsByTagName('article')[0];
+			if(submitFormWithAjax(this, article)){
+				return false;
+			}
+			return true;
+		}
 	}
 }
 addLoadEvent(prepareForms);
+
+function isFilled(field){
+	if(field.value.replace(' ', '').length == 0){
+		return false;
+	}
+	var placeholder = field.placeholder || field.getAttribute('placeholder');
+	return (field.value != placeholder);
+}
+
+function isEmail(field){
+	return (field.value.indexOf('@') != -1 && field.value.indexOf('.') != -1);
+}
+
+function validateForm(whichform){
+	for(var i=0; i<whichform.elements.length; i++){
+		var element = whichform.elements[i];
+		if(element.required == 'required'){
+			if(!isFilled(element)){
+				alert('Please fill in the '+element.name+' field.');
+				return false;
+			}
+		}
+		if(element.type == 'email'){
+			if(!isEmail(element)){
+				alert('The '+element.name+' field must be a valid email address.');
+				return false;
+			}
+		}
+	}
+	return true;
+}
+
+function getHTTPObject(){
+	if(typeof XMLHttpRequest == 'undefined'){
+		XMLHttpRequest = function(){
+			try {
+				return new ActiveXObject('Msxml2.XMLHTTP.6.0');
+			} catch (e) {
+			}
+			try {
+				return new ActiveXObject('Msxml2.XMLHTTP.3.0');
+			} catch (e) {
+			}
+			try {
+				return new ActiveXObject('Msxml2.XMLHTTP');
+			} catch (e) {
+			}
+			return false;
+		}
+	}
+	return new XMLHttpRequest();
+}
+
+function displayAjaxLoading(element){
+	while(element.hasChildNodes()){
+		element.removeChild(element.lastChild);
+	}
+	var content = document.createElement('img');
+	content.setAttribute('src', '../images/loading.gif');
+	content.setAttribute('alt', 'Loading...');
+	element.appendChild(content);
+}
+
+function submitFormWithAjax(whichform, thetarget){
+	var request = getHTTPObject();
+	if(!request){
+		return false;
+	}
+	displayAjaxLoading(thetarget);
+	
+	var dataParts = [];
+	var element = null;
+	for(var i=0; i<whichform.elements.length; i++){
+		element = whichform.elements[i];
+		dataParts[i] = element.name+'='+encodeURIComponent(element.value);
+	}
+	var data = dataParts.join('&');
+	request.open('POST', whichform.getAttribute('action'), true);
+	request.setRequestHeader('Content-type', 'application/x-www-from-urlencoded');
+	request.onreadystatechange = function(){
+		if(request.readyState == 4){
+			if(request.status == 200 || request.status == 0){
+				var matches = request.responseText.match(/<article>([\s\S]+)<\/article>/);
+				if(matches.length > 0){
+					thetarget.innerHTML = matches[1];
+				}else{
+					thetarget.innerHTML = '<p>Oops, there was an error. Sorry.</p>'
+				}
+			}else{
+				thetarget.innerHTML = '<p>'+request.statusText+'</p>'
+			}
+		}
+	}
+	request.send(data);
+	return true;
+}
